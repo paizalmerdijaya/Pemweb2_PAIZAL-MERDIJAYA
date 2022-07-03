95c03a8528a8a3d530a44774de7bf4353e90db4f
paizalmerdijaya <92238375+paizalmerdijaya@users.noreply.github.com>
2022-07-03 17:59:16 +0700

    Add files via upload

---
+++ praktikum_7/application/views/layout/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="<?= base_url() ?>">NAVBAR</a>
+            <span class="navbar-toggler-icon"></span>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link active" aria-current="page" href="<?= base_url() ?>">Home</a>
+                <a class="nav-link" href="<?= base_url() ?>mahasiswa">Mahasiswa</a>
+                <a class="nav-link" href="<?= base_url() ?>dosen">Dosen</a>
+                <a class="nav-link" href="<?= base_url() ?>matakuliah">Matakuliah</a>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ praktikum_7/application/views/pages/home.php
@@ -0,0 +1,8 @@
+<div class="cantainer mt-4 text-center">
+    <h2 class="display-6">Praktikum 7</h2>
+    <div class="mt-2">
+        <p class="m-0">Nama: PAIZAL MERDIJAYA</p>
+        <p class="m-0">NIM: 0110221014</p>
+        <p class="m-0">Rombel: TI16</p>
+    </div>
+</div>
